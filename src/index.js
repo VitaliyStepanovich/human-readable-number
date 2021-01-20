27a3b89872897eca6d1329297f9b11c399911705
VitaliyStepanovich <vitaliys.garshin@gmail.com>
2021-01-21 00:30:21 +0300

    feat: solved the task

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-module.exports = function toReadable (number) {
+module.exports = function toReadable (n) { 
 
-  
+  let hundreds = Math.floor(n / 100);
+  let tens = Math.floor((n - (hundreds * 100)) > 19 ? (n - (hundreds * 100)) / 10 : 0);
+  let units = n - (hundreds * 100) - (tens * 10);
+
+  const unitsAs = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const tensAs = ['','ten','twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  const scalesAs = ['hundred', 'thousand', 'million', 'billion'];
+
+  let readable =  (hundreds > 0 ? unitsAs[hundreds] + ' ' + scalesAs[0] + ' ' : '') +  (tens > 0 ?  tensAs[tens] + ' ' : '') +  (units > 0 ? unitsAs[units] : '');
+
+  return readable.trim() || 'zero';  
 }
